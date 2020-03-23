0a615c3beb60524cc17d780b752993ca4862b63d
Inderpreet Dhillon <DragynSlayr@users.noreply.github.com>
2020-03-23 15:07:31 -0600

    Added base files

---
+++ index.js
@@ -0,0 +1,29 @@
+const EVENTS = {
+    CONNECT: 'connect',
+    DISCONNECT: 'disconnect',
+};
+
+let express = require('express');
+let path = require('path');
+
+let app = express();
+let server = require('http').Server(app);
+let io = require('socket.io')(server);
+
+app.use('/', express.static('game'));
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname + '/game/login.html'));
+});
+
+server.listen(80, () => {
+    console.log('Game server started on port 80');
+});
+
+io.on(EVENTS.CONNECT, socket => {
+    console.log('Client connected');
+	
+	socket.on(EVENTS.DISCONNECT, () => {
+        console.log('Client disconnected');
+    });
+});
