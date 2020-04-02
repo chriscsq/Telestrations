cdd53e796d334247cec8b20f76cb7b0264ea7a2d
chris <chrischau94@gmail.com>
2020-04-02 16:14:07 -0600

    added functions

--- game_screen/func.js
+++ game_screen/func.js
@@ -1,3 +1,27 @@
 window.onload = () => {
   new Vue({ el: '#app' })
 }
+
+var userlist = new Vue({
+  el: "#userlist",
+  data: {
+    items: [
+      {name: "Chris"},
+      {name: "Gary" },
+      {name: "Hassan"},
+      {name: "Inderpreet"},
+      {name: "David"},
+      {name: "Chintav"},
+      {name: "Traitor"}
+    ]
+  }
+})
+
+var timer = new Vue({
+  el: "#timer",
+  data: function() {
+    return {
+      time: 100
+    }
+  }
+})
