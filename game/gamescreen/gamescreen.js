ea522a3f6cb03ebaaf0fff0da3c8c0428d5c8986
Inderpreet Dhillon <DragynSlayr@users.noreply.github.com>
2020-04-18 20:49:28 -0600

    Fixed start game and word selection

--- game/gamescreen/gamescreen.js
+++ game/gamescreen/gamescreen.js
@@ -1,5 +1,6 @@
-window.onload = async() => {
-  var vueMain = new Vue({ el: '#app', 
+window.onload = async () => {
+  var vueMain = new Vue({
+    el: '#app',
     data: {
       playerList: [''],
       timers: [''],
@@ -11,26 +12,26 @@ window.onload = async() => {
       this.getWordList();
     },
     methods: {
-      
+
       async getPlayerList() {
-        return  this.playerList = await getPlayersInRoom("GCWD");
+        return this.playerList = await getPlayersInRoom("GCWD");
       },
       async getTimeLimit() {
         return this.timers = await getTimeLimit("GCWD");
       },
-      async getWordList(){
+      async getWordList() {
         return this.wordList = await getWordList();
       },
     }
   })
 
   //startGame();
-} 
+}
 
 Vue.component("timercomponent", {
   template: "#timer-component",
   props: ['value']
-  
+
 })
 
 Vue.component("playerlist", {
@@ -39,40 +40,49 @@ Vue.component("playerlist", {
 
 })
 
+let socket = io();
+
+socket.on("connect", () => {
+  console.log("connected here");
+});
+
+socket.on("pickaword", function () {
+  document.getElementById("overlay").style.display = "block";
+});
+
+socket.on("updateTimer", function (data) {
+  document.getElementById("timer").innerHTML = data;
+});
+
 // start game called on click
 async function startGame() {
   //Socket connects to the namespace of the gameroom
   document.getElementById("startgame").style.display = 'none';
   var roomCode = Cookies.get(roomCode);
   //let socket = io('/'+ roomCode);
-  let socket = io();
   console.log('start game pressed');
 
+  let rounds = await getRoomLimit("GCWD");
+  let drawLimit = await getTimeLimit("GCWD");
 
-  var rounds = await console.log(Promise.resolve(getRoomLimit("GCWD")));
-  var drawLimit = await getTimeLimit("GCWD");
-
-  let roomInfo = {};
-  let setObj = async function(maxRounds, drawTime) {
-    roomInfo.maxRounds = maxRounds;
-    roomInfo.drawTime = drawTime;
-  }
-
-
-
-  socket.on("connect", () => {
-    console.log("connected here");
+  Promise.all([rounds, drawLimit]).then(() => {
+    let roomInfo = {
+      rounds,
+      drawLimit,
+    };
     socket.emit('loadGame', roomInfo);
-  })
-
-  socket.on("pickaword", function() {
-    document.getElementById("overlay").style.display = "block";
   });
 
-  socket.on("updateTimer", function(data) {
-    document.getElementById("timer").innerHTML = data;
-  })
+  // var rounds = await getRoomLimit("GCWD");
+  // console.log('Rounds after promise', rounds);
+  // var drawLimit = await getTimeLimit("GCWD");
+  // console.log('Draw limit after promise', drawLimit[0]);
 
+  // let roomInfo = {};
+  // let setObj = async function (maxRounds, drawTime) {
+  //   roomInfo.maxRounds = maxRounds;
+  //   roomInfo.drawTime = drawTime;
+  // }
 }
 
 async function getAsyncRoomLimit(roomCode) {
@@ -80,20 +90,20 @@ async function getAsyncRoomLimit(roomCode) {
 }
 
   //pickTimer();
-  /*
-  while (game_over != true) {
+/*
+while (game_over != true) {
 
-    if (round == roundLimit) {
-        game_over = true;
-    }
-    pickWord(round);
+  if (round == roundLimit) {
+      game_over = true;
+  }
+  pickWord(round);
 
-    console.log(round);
-      //pickWord();
+  console.log(round);
+    //pickWord();
 
-    /* have people pick a word for 15 seconds */
-  
-    /* draw for 50 seconds */
-  
-    /* guess for 30 seconds */
-  
+  /* have people pick a word for 15 seconds */
+
+/* draw for 50 seconds */
+
+/* guess for 30 seconds */
+
--- index.js
+++ index.js
@@ -211,47 +211,45 @@ io.on('connect', socket => {
     socket.on('disconnect', () => {
         console.log('Client disconnected');
     });
-    
+
 
     /* Gamescreen */
-    
+
     let gameOver = false;
     let currentRound = 0;
-    socket.on("loadGame", function(data) {
+    socket.on("loadGame", function (data) {
         let maxRounds = data;
 
         /* needs to get async values, right now they are hardcoded */
-        let pickWordTimer = 5; // in seconds
-        var drawTime = 10;
+        let pickWordTimer = 15; // in seconds
+        let drawTime = data.drawLimit[0];
+        console.log('Loaded stats: ', data);
 
         io.emit("pickaword");
         /* Function to set the time */
         setTimer(pickWordTimer, "pick");
-        
+
         /* Here is where we get the round time */
         setTimeout(() => {
             setTimer(drawTime, "draw");
-        }, (pickWordTimer+1)*1000);
+        }, (pickWordTimer + 1) * 1000);
     });
 
     function setTimer(time, timertype) {
-        var refresh = setInterval(function() {
+        var refresh = setInterval(function () {
             if (time == 0) {
-              if (timertype == "pick") {
-                io.emit("updateTimer", "DRAW!");
-              } else {
-                io.emit("updateTimer", "Time's up");
-              }
-              clearInterval(refresh);
-
+                if (timertype === "pick") {
+                    io.emit("updateTimer", "DRAW!");
+                } else {
+                    io.emit("updateTimer", "Time's up");
+                }
+                clearInterval(refresh);
             } else {
-              console.log("your timer: " + time);
-              io.emit('updateTimer', time);
+                console.log("your timer: " + time);
+                io.emit('updateTimer', time);
             }
             time -= 1;
-          }, 1000);
+        }, 1000);
     }
-
-
 });
 
